068d9368afce2a54989cf3c8ae9b89ddfd8b6428
jygahallon <jygahallon@student.apc.edu.ph>
2019-02-26 13:12:42 +0800

    final

---
+++ cardreaderb/cardreaderb/cardreaderb.ino
@@ -0,0 +1,232 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <WiFiEsp.h>
+#include <ArduinoJson.h>
+
+#define SS_PIN 53 
+#define RST_PIN 5
+
+char ssid[] = "ml";    
+char pwd[] = "mshsrondalla";  
+String cardNumber[100];
+String terminal_id="1";
+String url;
+String line;  
+WiFiEspClient client;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);        
+MFRC522::MIFARE_Key key;
+int block=2;
+byte readbackblock[18];
+String card_number;
+
+bool queue=false;
+int jeepCapacity=0;
+int queued=0;
+String jeepId;
+void setup(){
+  Serial.begin(19200);
+  Serial1.begin(9600);
+
+  SPI.begin();               
+  mfrc522.PCD_Init();
+  for (byte i = 0; i < 6; i++) 
+  {
+    key.keyByte[i] = 0xFF;
+    }
+ WiFi.init(&Serial1);
+ while (WiFi.status() != WL_CONNECTED)
+ {
+   WiFi.begin(ssid, pwd); 
+
+ }
+}
+
+void loop(){
+  if(!queue)
+  {
+    jeepCapacity=trackingJeep();
+    queued=jeepCapacity;
+    if(jeepCapacity!=0)
+    {
+      Serial.print("capacity");
+      Serial.println(jeepCapacity);
+      queue=true;
+      return;
+    }
+  }
+  else
+  {
+      if ( !mfrc522.PICC_IsNewCardPresent()) 
+      {
+        return;
+      }
+      if ( ! mfrc522.PICC_ReadCardSerial()) 
+      {
+        return;
+      }
+      readBlock(block, readbackblock);//read the block back
+      card_number = (char*)readbackblock;
+      Serial.println(card_number);
+      String response = checkCard();
+      if (response.equals("true"))
+      {
+        //green led
+       
+      }
+      else if (response.equals("full"))
+      {
+        //green led
+        queue=false;
+        return;
+        
+      }
+      else if (response.equals("false"))
+      {
+        //red led
+      }
+//
+//    if(queued!=0)
+//    {
+//     if ( !mfrc522.PICC_IsNewCardPresent()) 
+//     {
+//      return;
+//      }
+//      if ( ! mfrc522.PICC_ReadCardSerial()) 
+//      {
+//        return;
+//      }
+//      readBlock(block, readbackblock);//read the block back
+//      card_number = (char*)readbackblock;
+//      Serial.println(card_number);
+//      if (checkCard())
+//      {
+//        Serial.println("found");
+//      }
+//      else
+//      {
+//        Serial.println("not found");
+//      }
+//    }
+//    else
+//    {
+//      Serial.println("EMPTY QUEUE");
+//      deQueueJeep();
+//      queue=false;
+//    }
+  }
+}
+
+int readBlock(int blockNumber, byte arrayAddress[]) 
+{
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;
+
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+ 
+  if (status != MFRC522::STATUS_OK) {
+         Serial.print("PCD_Authenticate() failed (read): ");
+         Serial.println(mfrc522.GetStatusCodeName(status));
+         return 3;
+  }
+      
+  byte buffersize = 18;
+  status = mfrc522.MIFARE_Read(blockNumber, arrayAddress, &buffersize);
+  if (status != MFRC522::STATUS_OK) {
+          Serial.print("MIFARE_read() failed: ");
+          Serial.println(mfrc522.GetStatusCodeName(status));
+          return 4;
+  }
+  Serial.println("block was read");
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+}
+void clientConnect()
+{
+  while(!client.connected())
+  {
+      client.connect("206.189.209.210", 80);
+
+  }
+}
+int trackingJeep(){
+  clientConnect();
+  url="GET /jt/public/jeep/tracking/"+terminal_id+" HTTP/1.1";
+  Serial.print("URL");
+  Serial.println(url);
+////  //client.println(url);
+
+
+  client.print(url);
+  client.println();
+  client.println("Host: 206.189.209.210");
+  client.println("Connection: close");
+
+  client.println();
+  Serial.println("Response");
+
+  line = client.readStringUntil("}");
+  client.flush();
+  client.stop();
+  int start = line.indexOf('{');
+  line=line.substring(start,line.length());
+  Serial.println(line);
+  if((line.equals("{}"))||(line.equals("")))
+  {
+    Serial.println("empty");
+    return 0;
+  }
+    
+ const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+  DynamicJsonBuffer jsonBuffer(capacity);
+  JsonObject& root = jsonBuffer.parseObject(line);
+  String on_location=root["on_location"].as<char*>();
+  Serial.print("on_location");
+  Serial.println(on_location);
+  //return false;
+  if (on_location.equals("0"))
+  {
+      return 0;
+  }
+  else
+ {
+    jeepId=root["id"].as<char*>();
+    return root["capacity"].as<int>();
+//    //capacity=(String)cap;
+//    Serial.print("Capacity");
+//    Serial.println(capacity);
+//    return true;
+ }
+}
+
+String checkCard()
+{
+  clientConnect();
+   url="GET /jt/public/terminal/queueRide/"+terminal_id+"/"+jeepId+"/"+card_number+" HTTP/1.1";
+  Serial.print("URL");
+  Serial.println(url);
+////  //client.println(url);
+
+
+  client.print(url);
+  client.println();
+  client.println("Host: 206.189.209.210");
+  client.println("Connection: close");
+
+  client.println();
+  Serial.println("Response");
+
+  line = client.readStringUntil("}");
+  client.flush();
+  client.stop();
+  int start = line.indexOf('{');
+  line=line.substring(start,line.length());
+  Serial.println(line);
+
+  const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+  DynamicJsonBuffer jsonBuffer(capacity);
+  JsonObject& root = jsonBuffer.parseObject(line);
+  String response=root["message"].as<char*>();
+  return response;
+ 
+}
